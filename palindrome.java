188e474446cf6c0b17e3dc6a97c3be1c0d22fcb7
shruthikurva <kurvashruthi98@gmail.com>
2025-01-30 21:53:09 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+
+public class palindrome{
+    public static void main (String[]args){
+        int r,sum=0,temp;
+        int n=1221;
+        temp=n;
+        while(n<0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+if(temp==sum)
+System.out.println( "palindrome number");
+else
+System.out.println("not a palindrome number");
+    }
+}
